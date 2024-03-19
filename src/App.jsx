b826772108794e1a9e81c2196394ca4902648bd6
xClune <aclune382@gmail.com>
2024-03-19 12:38:50 +0800

    Add 'No Todos' on empty list.

--- src/App.jsx
+++ src/App.jsx
@@ -55,6 +55,7 @@ export default function App() {
 
       <h1 className="header">Todo List</h1>
       <ul className="list">
+        {todos.length === 0 && "No Todos"}
         {todos.map(todo => {
           return (
             <li key={todo.id}>
