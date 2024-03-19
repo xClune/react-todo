ba225abf46e8c863915930b4ab54ba422537e768
xClune <aclune382@gmail.com>
2024-03-19 11:41:36 +0800

    Remove template files.
    Add premade css file.
    
    Create static layout for todo.
    
    Add state to text input. setNewItem onChange in input.

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+
+import { useState } from "react"
+import "./styles.css"
+
+export default function App() {
+  const [newItem, setNewItem] = useState("")
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input 
+          value={newItem} 
+          onChange={e => setNewItem(e.target.value)} type="text" 
+          id="item"/>
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label htmlFor="">
+          <input type="checkbox" />
+          Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label htmlFor="">
+          <input type="checkbox" />
+          Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label htmlFor="">
+          <input type="checkbox" />
+          Item 3
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  )
+}
